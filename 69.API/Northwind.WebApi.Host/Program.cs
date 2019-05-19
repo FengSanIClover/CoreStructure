fc5cf8e524616f14dab103fff73ab610ee8d92ea
CloverFeng <sunkesi02@gmail.com>
2019-05-19 23:13:30 +0800

    修改 Program

--- 69.API/Northwind.WebApi.Host/Program.cs
+++ 69.API/Northwind.WebApi.Host/Program.cs
@@ -7,18 +7,37 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Unity;
+using Unity.Microsoft.DependencyInjection;
 
 namespace Northwind.WebApi.Host
 {
     public class Program
     {
+        private static IUnityContainer _container;
         public static void Main(string[] args)
         {
+            //FunctionsAssemblyResolver.RedirectAssembly();
+            // Manully create Unity container
+#if DEBUG
+            _container = new UnityContainer()
+                // Optionally you could enable diagnostic extension
+                .AddExtension(new Diagnostic());
+#else
+            _container = new UnityContainer();
+#endif
             CreateWebHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+            .ConfigureAppConfiguration(
+                (webHostBuilder,configurationBinder) => 
+                {
+                configurationBinder.AddJsonFile("appsetting.json", optional: true);
+                }
+            )
+            .UseUnityServiceProvider(_container)
+            .UseStartup<Startup>();
     }
 }
